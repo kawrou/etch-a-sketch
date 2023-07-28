6baf2919e0ea74fe962f69ebac70fcd933c71bd1
kawrou <121672581+kawrou@users.noreply.github.com>
2023-07-28 13:12:02 +0100

    Created a single box/div element using JS and css
    Tested to see it works by styling it, giving it height, width, and
    background-color.

---
+++ script.js
@@ -0,0 +1,4 @@
+const GRID_CONTAINER = document.querySelector(".grid-container");
+let gridBox = document.createElement("div");
+gridBox.classList.add("js-grid-box")
+GRID_CONTAINER.appendChild(gridBox);
