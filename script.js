252c6f550e880eaa13d45832b3055bfe7b14952b
kawrou <121672581+kawrou@users.noreply.github.com>
2023-07-31 18:09:41 +0100

    Testing if etch-a-sketch works

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const GRID_BUTTON = document.getElementById("grid-button");
-const CLEAR_BUTTON = document.getElementById("clear-button"); 
+const GRID_BUTTON = document.querySelector("button");
+const CLEAR_BUTTON = document.getElementById("clear-button")
 const GRID_CONTAINER = document.querySelector(".grid-container");
 
 function drawGrid(gridSize){
@@ -26,39 +26,39 @@ let getGridSize = function(){
         alert("Please input up to 100 only")
     }
     else {
-    makeRowCol(gridSize); 
-    gridSize *= gridSize
-    drawGrid(gridSize); 
+        makeRowCol(gridSize); 
+        gridSize *= gridSize
+        drawGrid(gridSize); 
     }
-}
+}; 
 
 GRID_BUTTON.addEventListener("click", getGridSize); 
 
-// Need some explanation for this code. 
-// let htmlStyles = window.getComputedStyle(document.querySelector("html"));
-// let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
-// let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
+let htmlStyles = window.getComputedStyle(document.querySelector("html"));
+let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
+let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
 
-// Need some explanation about this code. 
 function makeRowCol(gridSize){
     document.documentElement.style.setProperty("--rowNum", gridSize); 
     document.documentElement.style.setProperty("--colNum", gridSize); 
-}
+}; 
 
-CLEAR_BUTTON.addEventListener("click",clearGrid); 
-    
+CLEAR_BUTTON.addEventListener("click", clearGrid); 
 
 function clearGrid(){
-    let removeElm = GRID_CONTAINER.querySelectorAll(".js-grid-item--fill-black"); 
+    let removeElm = GRID_CONTAINER.querySelectorAll(".js-grid-item--fill-black");
     removeElm.forEach(n=> n.classList.toggle("js-grid-item--fill-black")); 
-    
-    // Question - Can use the forEach array method with selecting that collection of DOM?
-    // GRID_CONTAINER.querySelectorAll("js-grid-item--fill-black").forEach(n => n.classList.toggle("js-grid-item--fill-black")); 
 
+    // Question - Can use the forEach array method with selecting that collection of DOM?
+    // GRID_CONTAINER.querySelectorAll("div").forEach(n => n.remove()); 
+    // GRID_CONTAINER.querySelectorAll("js-grid-item--fill-black").forEach(n => n.classList.toggle("js-grid-item--fill-black")); 
+    
     // Looking online, it says will get an HTML collection
-    // GRID_CONTAINER.querySelectorAll(".js-grid-item--fill-black").classList.toggle("js-grid-item--fill-black"); 
+     // GRID_CONTAINER.querySelectorAll(".js-grid-item--fill-black").classList.toggle("js-grid-item--fill-black"); 
     
     // let removeElm = GRID_CONTAINER.querySelectorAll(".js-grid-item"); 
+    // removeElm.remove(); - doesn't work - returns not a function. 
+    // removeElm.forEach(n => n.remove()); 
     // removeElm.remove(); -- not a function
     // removeElm.forEach(n => n.remove()); -- removes all DIV elements  
 }; 
