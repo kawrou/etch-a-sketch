d6bf05f05b48f6927f5fc3ee78613af21c661296
kawrou <121672581+kawrou@users.noreply.github.com>
2023-07-31 11:16:48 +0100

    Button successfully populates the GRID_CONTAINER
    I solved the previous issue by placing the "for loop" that draws the
    div boxes inside a function called drawGrid.
    Then call drawGrid inside the getGridSize function.
    The getGridSize function then returns the users input into the drawGrid
    function.
    Only problem is how to dynamically size each div/gridItem to fill out
    the GRID_CONTAINER.
    I'm reading about CSS variables that might help solve the issue.

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
 const GRID_BUTTON = document.querySelector("button");
 const GRID_CONTAINER = document.querySelector(".grid-container");
 
-for (i = 0; i < 256; i++){
+function drawGrid(gridSize){
+for (i = 0; i < gridSize; i++){
     let gridItem = document.createElement("div");
     gridItem.classList.add("js-grid-item")
     gridItem.setAttribute("id", i);
     GRID_CONTAINER.appendChild(gridItem);
 }
-
+}; 
 // GRID_CONTAINER.addEventListener("mouseover", (e)=>console.log(e));
 
-GRID_CONTAINER.addEventListener("click", function (e) {
+GRID_CONTAINER.addEventListener("mouseover", function (e) {
     if (e.target.id){
         e.target.classList.add("js-grid-item--fill-black")
     }});
 
-// let gridSize = ""; 
-// console.log(gridSize); 
-
 let getGridSize = function(){
     let gridSize = prompt("Enter number");
-    console.log(gridSize); 
-    return; 
+    gridSize *= gridSize
+    drawGrid(gridSize); 
 }
 
 GRID_BUTTON.addEventListener("click", getGridSize); 
-
-// GRID_BUTTON.addEventListener("click", () => 
-// {
-//     gridSize = prompt("Enter number.");
-//     console.log(gridSize);
-//     return;
-// }); 
-
-
