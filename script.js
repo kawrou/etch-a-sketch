e62f50673dd2bc44ffb68b68f1a0cbade698dd52
kawrou <121672581+kawrou@users.noreply.github.com>
2023-07-31 19:31:53 +0100

    Retesting code to draw new grids into container

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const GRID_BUTTON = document.getElementById("grid-button");
-const CLEAR_BUTTON = document.getElementById("clear-button"); 
 const GRID_CONTAINER = document.querySelector(".grid-container");
 
 function drawGrid(gridSize){
@@ -44,21 +43,3 @@ function makeRowCol(gridSize){
     document.documentElement.style.setProperty("--rowNum", gridSize); 
     document.documentElement.style.setProperty("--colNum", gridSize); 
 }
-
-CLEAR_BUTTON.addEventListener("click",clearGrid); 
-    
-
-function clearGrid(){
-    let removeElm = GRID_CONTAINER.querySelectorAll(".js-grid-item--fill-black"); 
-    removeElm.forEach(n=> n.classList.toggle("js-grid-item--fill-black")); 
-    
-    // Question - Can use the forEach array method with selecting that collection of DOM?
-    // GRID_CONTAINER.querySelectorAll("js-grid-item--fill-black").forEach(n => n.classList.toggle("js-grid-item--fill-black")); 
-
-    // Looking online, it says will get an HTML collection
-    // GRID_CONTAINER.querySelectorAll(".js-grid-item--fill-black").classList.toggle("js-grid-item--fill-black"); 
-    
-    // let removeElm = GRID_CONTAINER.querySelectorAll(".js-grid-item"); 
-    // removeElm.remove(); -- not a function
-    // removeElm.forEach(n => n.remove()); -- removes all DIV elements  
-}; 
