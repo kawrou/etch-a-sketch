9e9da502b2f60a6cf402c7a7e4c249b9dc0d2012
kawrou <121672581+kawrou@users.noreply.github.com>
2023-08-01 03:34:55 +0100

    Fixed issue with changing grid size
    Added a function for removing grid child divs before redrawing new
    grid.
    Placed back the code for clearing drawing and tested both buttons work.
    Tidied up the order of code for better legibility.

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
 const GRID_BUTTON = document.querySelector("button");
-// const CLEAR_BUTTON = document.getElementById("clear-button")
+const CLEAR_BUTTON = document.getElementById("clear-button")
 const GRID_CONTAINER = document.querySelector(".grid-container");
 
+//Event listener for filling in black squares. 
+// GRID_CONTAINER.addEventListener("mouseover", (e)=>console.log(e));
+GRID_CONTAINER.addEventListener("mouseover", function (e) {
+    if (e.target.id){
+        e.target.classList.add("js-grid-item--fill-black")
+    }});
+
+//Event listener for the button that changes the grid size.
+GRID_BUTTON.addEventListener("click", getGridSize); 
+
+let getGridSize = function(){
+    let gridSize = prompt("Enter number");
+    if (isNaN(parseInt(gridSize))){
+        alert("Please input digits only")
+        return; 
+    } 
+    else if (gridSize > 100){
+        alert("Please input up to 100 only")
+        return; 
+    }
+    else {
+        removeGrid(); 
+        makeRowCol(gridSize); 
+        drawGrid(gridSize); 
+    }
+}; 
+
+//Function that is called to remove current grid before drawing new grid. 
+function removeGrid(){
+    let gridChildAll = GRID_CONTAINER.querySelectorAll(".js-grid-item"); 
+    gridChildAll.forEach(n => n.remove()); 
+}
+
+// let htmlStyles = window.getComputedStyle(document.querySelector("html"));
+// let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
+// let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
+
+//Setting rows and columns of grid
+function makeRowCol(gridSize){
+    document.documentElement.style.setProperty("--rowNum", gridSize); 
+    document.documentElement.style.setProperty("--colNum", gridSize); 
+}; 
+
+//Loop for populating grid with div
 function drawGrid(gridSize){
+gridSize *= gridSize
 for (i = 0; i < gridSize; i++){
     let gridItem = document.createElement("div");
     gridItem.classList.add("js-grid-item")
@@ -10,44 +55,13 @@ for (i = 0; i < gridSize; i++){
     GRID_CONTAINER.appendChild(gridItem);
 }
 }; 
-// GRID_CONTAINER.addEventListener("mouseover", (e)=>console.log(e));
 
-GRID_CONTAINER.addEventListener("mouseover", function (e) {
-    if (e.target.id){
-        e.target.classList.add("js-grid-item--fill-black")
-    }});
+//Clear button for removing drawings. 
+CLEAR_BUTTON.addEventListener("click", clearGrid); 
 
-let getGridSize = function(){
-    let gridSize = prompt("Enter number");
-    if (isNaN(parseInt(gridSize))){
-        alert("Please input digits only")
-    } 
-    else if (gridSize > 100){
-        alert("Please input up to 100 only")
-    }
-    else {
-        makeRowCol(gridSize); 
-        gridSize *= gridSize
-        drawGrid(gridSize); 
-    }
-}; 
-
-GRID_BUTTON.addEventListener("click", getGridSize); 
-
-let htmlStyles = window.getComputedStyle(document.querySelector("html"));
-let rowNum = parseInt(htmlStyles.getPropertyValue("--rowNum"));
-let colNum = parseInt(htmlStyles.getPropertyValue("--colNum"));
-
-function makeRowCol(gridSize){
-    document.documentElement.style.setProperty("--rowNum", gridSize); 
-    document.documentElement.style.setProperty("--colNum", gridSize); 
-}; 
-
-// CLEAR_BUTTON.addEventListener("click", clearGrid); 
-
-// function clearGrid(){
-//     let removeElm = GRID_CONTAINER.querySelectorAll(".js-grid-item--fill-black");
-//     removeElm.forEach(n=> n.classList.toggle("js-grid-item--fill-black")); 
+function clearGrid(){
+    let removeElm = GRID_CONTAINER.querySelectorAll(".js-grid-item--fill-black");
+    removeElm.forEach(n=> n.classList.toggle("js-grid-item--fill-black")); 
 
     // Question - Can use the forEach array method with selecting that collection of DOM?
     // GRID_CONTAINER.querySelectorAll("div").forEach(n => n.remove()); 
@@ -61,4 +75,4 @@ function makeRowCol(gridSize){
     // removeElm.forEach(n => n.remove()); 
     // removeElm.remove(); -- not a function
     // removeElm.forEach(n => n.remove()); -- removes all DIV elements  
-// }; 
+}; 
